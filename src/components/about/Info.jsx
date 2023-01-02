838f4c354ce582e1eacff9015fb6efd42ce0f7b0
Juanjo Ruiz <juanjoserusa@gmail.com>
2023-01-02 19:51:52 +0100

    segundo commit añadido servicios

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -5,22 +5,22 @@ const Info = () => {
     <div>
       <div className='about__info grid'>
         <div className='about__box'>
-        <i class='bx bx-award'></i>
+        <i class='bx bx-award about__icon'></i>
 
             <h3 className='about__title'>Experience</h3>
             <span className='about__subtitle'>17 years working in Graphic Design, 1 year with Code</span>
         </div>
 
         <div className='about__box'>
-        <i class='bx bx-briefcase-alt' ></i>
+        <i class='bx bx-briefcase-alt about__icon' ></i>
 
             <h3 className='about__title'>Completed</h3>
             <span className='about__subtitle'>20 + Projects</span>
         </div>
 
         <div className='about__box'>
-        <i class='bx bx-support' ></i>
-        
+        <i class='bx bx-support about__icon' ></i>
+
             <h3 className='about__title'>Support</h3>
             <span className='about__subtitle'>Online 24/7</span>
         </div>
