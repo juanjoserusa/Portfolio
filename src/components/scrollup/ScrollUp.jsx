0809c310e6d6c234b71aefdc088ba239bd1ba164
Juan Jose Ruiz Sanchez <juanjoserusa@gmail.com>
2023-01-04 13:30:34 +0100

    Terminado ventana portfolio

--- src/components/scrollup/ScrollUp.jsx
+++ src/components/scrollup/ScrollUp.jsx
@@ -3,7 +3,7 @@ import './scrollup.css'
 
 const ScrollUp = () => {
 
-    window.addEventListener("scroll", function () {
+    window.addEventListener("scroll", function() {
         const scrollUp = document.querySelector(".scrollup");
         // when the scroll is higher than 560 viewport height, add the show-scroll class to a tag with the scroll-top class
         if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
@@ -11,6 +11,7 @@ const ScrollUp = () => {
     });
     
   return (
+    // eslint-disable-next-line jsx-a11y/anchor-is-valid
     <a href='#' className='scrollup'>
         <i className='uil uil-arrow-up scrollup__icon'></i>
     </a>
---
+++ src/components/work/Data.jsx
@@ -0,0 +1,52 @@
+import Work1 from '../../assets/work1.jpg'
+import Work2 from '../../assets/work2.jpg'
+import Work3 from '../../assets/work3.jpg'
+import Work4 from '../../assets/work4.jpg'
+import Work5 from '../../assets/work5.jpg'
+
+
+
+export const projectsData =[
+    {
+        id: 1,
+        image: Work1,
+        title: 'Title 1',
+        category: 'react native',
+    },
+    {
+        id: 2,
+        image: Work2,
+        title: 'Title 2',
+        category: 'react',
+    },{
+        id: 3,
+        image: Work3,
+        title: 'Title 3',
+        category: 'react',
+    },{
+        id: 4,
+        image: Work4,
+        title: 'Title 4',
+        category: 'design',
+    },{
+        id: 5,
+        image: Work5,
+        title: 'Title 5',
+        category: 'react native',
+    },
+];
+
+export const projectNav = [
+    {
+        name:'all',
+    },
+    {
+        name:'react',
+    },
+    {
+        name:'react native',
+    },
+    {
+        name:'design',
+    },
+];
