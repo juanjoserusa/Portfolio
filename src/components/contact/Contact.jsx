9b6d06ac73856f3bdf565884d28b6b386027cdfd
Juan Jose Ruiz Sanchez <juanjoserusa@gmail.com>
2023-01-04 11:14:24 +0100

    añadido contact e integrado el sistema de envio de email

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import "./contact.css";
 
 const Contact = () => {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_sm3kg4l', 'template_a1f2tyj', form.current, '1OODOvdJhLrZmO6wX')
+      
+    e.target.reset()
+  };
+
   return (
     <section className='contact section' id='contact'>
         <h2 className="section__title">Contact Me</h2>
@@ -43,7 +55,7 @@ const Contact = () => {
 
         <div className='contact__content'>
             <h3 className='contact__title'>Write me your project</h3>
-            <form className='contact__form'>
+            <form ref={form} onSubmit={sendEmail} className='contact__form'>
                 <div className='contact__form-div'>
                     <label className='contact__form-tag'>Name</label>
                     <input type="text" name="name" className='contact__form-input' placeholder='Insert your name' />
